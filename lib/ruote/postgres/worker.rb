810f2786e94ecefb613b515bd083974b30fb5560
Lleïr Borràs Metje <lleir@llegeix.me>
2013-03-13 12:01:07 +0100

    Update timeout to 60 seconds

--- lib/ruote/postgres/worker.rb
+++ lib/ruote/postgres/worker.rb
@@ -2,7 +2,7 @@ require 'ruote/worker'
 
 Ruote::Worker.class_eval do
   def run
-    @storage.wait_for_notify(6) do
+    @storage.wait_for_notify(60) do
       step
     end while @state != 'stopped'
   end
