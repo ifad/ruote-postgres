ddd24e5f2d74538308d61f19fdb70560f8c8762c
Luca Spiller <luca@blissfulsystems.com>
2016-04-05 11:09:17 +0200

    Convert specs to RSpec 3.4.4 syntax with Transpec
    This conversion is done by Transpec 3.2.2 with the following command:
        transpec -f .
    
    * 29 conversions
        from: obj.should
          to: expect(obj).to
    
    * 12 conversions
        from: =~ [1, 2]
          to: match_array([1, 2])
    
    * 11 conversions
        from: == expected
          to: eq(expected)
    
    * 3 conversions
        from: be_true
          to: be_truthy
    
    * 2 conversions
        from: Klass.any_instance.should_receive(:message)
          to: expect_any_instance_of(Klass).to receive(:message)
    
    * 1 conversion
        from: Klass.any_instance.stub(:message)
          to: allow_any_instance_of(Klass).to receive(:message)
    
    * 1 conversion
        from: be_false
          to: be_falsey
    
    * 1 conversion
        from: obj.should_not
          to: expect(obj).not_to
    
    * 1 conversion
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/unit/storage_spec.rb
+++ spec/unit/storage_spec.rb
@@ -10,36 +10,36 @@ describe Ruote::Postgres::Storage do
 
   describe "#initialize" do
     before do
-      Ruote::Postgres::Storage.any_instance.stub(:replace_engine_configuration)
+      allow_any_instance_of(Ruote::Postgres::Storage).to receive(:replace_engine_configuration)
     end
 
     it "prepares the storage" do
-      Ruote::Postgres::Storage.any_instance.should_receive(:replace_engine_configuration)
+      expect_any_instance_of(Ruote::Postgres::Storage).to receive(:replace_engine_configuration)
       Ruote::Postgres::Storage.new(pg)
     end
 
     it "prepares the storage with options" do
       options = {a: :b}
 
-      Ruote::Postgres::Storage.any_instance.should_receive(:replace_engine_configuration).with(options)
+      expect_any_instance_of(Ruote::Postgres::Storage).to receive(:replace_engine_configuration).with(options)
       Ruote::Postgres::Storage.new(pg, options)
     end
 
     it "assigns the db connection" do
       storage = Ruote::Postgres::Storage.new(pg)
-      storage.instance_variable_get(:@pg).should == pg
+      expect(storage.instance_variable_get(:@pg)).to eq(pg)
     end
 
     it "assigns the table with default value" do
       storage = Ruote::Postgres::Storage.new(pg)
-      storage.instance_variable_get(:@table).should == :documents
+      expect(storage.instance_variable_get(:@table)).to eq(:documents)
     end
 
     it "assigns the table name" do
       options = {'pg_table_name' => "some_table_name"}
 
       storage = Ruote::Postgres::Storage.new(pg, options)
-      storage.instance_variable_get(:@table).should == :some_table_name
+      expect(storage.instance_variable_get(:@table)).to eq(:some_table_name)
     end
   end
 
@@ -50,24 +50,24 @@ describe Ruote::Postgres::Storage do
       context "with new document" do
         it "works" do
           p subject.put({ "type" => "expressions", "_id" => "1" })
-          pg.query("select * from documents").values.should_not be_empty
+          expect(pg.query("select * from documents").values).not_to be_empty
         end
       end
 
       it "returns true if the document has been deleted from the store" do
-        subject.put({"type" => "expressions", "_id" => "4", "_rev" => 1}).should be_true
+        expect(subject.put({"type" => "expressions", "_id" => "4", "_rev" => 1})).to be_truthy
       end
 
       it "returns a document if the revision has changed" do
         doc = insert(pg, table_name, typ: 'expressions', ide: '4', wfid: "ef5678", rev: 2, doc: {"e" => "f"})
 
-        subject.put({"type" => "expressions", "_id" => "4", "_rev" => 1}).should == doc
+        expect(subject.put({"type" => "expressions", "_id" => "4", "_rev" => 1})).to eq(doc)
       end
 
       it "returns nil when the document is successfully stored" do
         insert(pg, table_name, typ: 'expressions', ide: '4', wfid: "ef5678", rev: 1, doc: {"e" => "f"})
 
-        subject.put({"type" => "expressions", "_id" => "4", "_rev" => 1}).should be_nil
+        expect(subject.put({"type" => "expressions", "_id" => "4", "_rev" => 1})).to be_nil
       end
 
     end
@@ -76,7 +76,7 @@ describe Ruote::Postgres::Storage do
       it "returns the document that matches the given type ans key" do
         doc =  insert(pg, table_name, typ: 'msgs', ide: '1')
 
-        subject.get('msgs', '1').should == doc
+        expect(subject.get('msgs', '1')).to eq(doc)
       end
     end
 
@@ -88,17 +88,17 @@ describe Ruote::Postgres::Storage do
       end
 
       it "returns true if already deleted" do
-        subject.delete({"type" => "expressions", "_id" => "4", "_rev" => 1}).should be_true
+        expect(subject.delete({"type" => "expressions", "_id" => "4", "_rev" => 1})).to be_truthy
       end
 
       it "returns a document if the revision of the given document doesn't match the version of the stored document" do
         doc = insert(pg, table_name, typ: 'expressions', ide: '4', wfid: "ef5678", rev: 2, doc: {"e" => "f"})
 
-        subject.delete({"type" => "expressions", "_id" => "4", "_rev" => 1}).should == doc
+        expect(subject.delete({"type" => "expressions", "_id" => "4", "_rev" => 1})).to eq(doc)
       end
 
       it "returns nil when successfully removed" do
-        subject.delete({"type" => "expressions", "_id" => "3", "_rev" => 1}).should be_nil
+        expect(subject.delete({"type" => "expressions", "_id" => "3", "_rev" => 1})).to be_nil
       end
     end
 
@@ -111,50 +111,50 @@ describe Ruote::Postgres::Storage do
 
       describe "without opts" do
         it "return an array of matching documents whitout any opts" do
-          subject.get_many("expressions").should =~ [{"type" => "expressions", "_id" => "3", "_rev" => 1, "c" => "d"},
-                                                     {"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}]
+          expect(subject.get_many("expressions")).to match_array([{"type" => "expressions", "_id" => "3", "_rev" => 1, "c" => "d"},
+                                                     {"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}])
         end
       end
 
       describe "with opts" do
         describe ":count" do
           it "returns the number of matching documents without explicit key as an integer" do
-            subject.get_many("expressions", nil, count: true).should == 2
+            expect(subject.get_many("expressions", nil, count: true)).to eq(2)
           end
 
           it "returns the number of matching documents with a matching key as an integer" do
-            subject.get_many("expressions", "cd34", count: true).should == 1
+            expect(subject.get_many("expressions", "cd34", count: true)).to eq(1)
           end
         end
 
         describe ":descending" do
           it "returns the list sorted by ide descending" do
-            subject.get_many("expressions", nil, descending: true).should =~ [{"type" => "expressions", "_id" => "3", "_rev" => 1, "c" => "d"},
-                                                                              {"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}]
+            expect(subject.get_many("expressions", nil, descending: true)).to match_array([{"type" => "expressions", "_id" => "3", "_rev" => 1, "c" => "d"},
+                                                                              {"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}])
           end
 
           it "returns the list sorted by ide descending with an explicit key" do
-            subject.get_many("expressions", "cd34", descending: true).should =~ [{"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}]
+            expect(subject.get_many("expressions", "cd34", descending: true)).to match_array([{"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}])
           end
         end
 
         describe ":skip" do
           it "returns the list skiping some results" do
-            subject.get_many("expressions", nil, skip: 1).should =~ [{"type" => "expressions", "_id" => "3", "_rev" => 1, "c" => "d"}]
+            expect(subject.get_many("expressions", nil, skip: 1)).to match_array([{"type" => "expressions", "_id" => "3", "_rev" => 1, "c" => "d"}])
           end
 
           it "returns the list skiping some reslts with an explicit key" do
-            subject.get_many("expressions", "cd34", skip: 1).should =~ []
+            expect(subject.get_many("expressions", "cd34", skip: 1)).to match_array([])
           end
         end
 
         describe ":limit" do
           it "returns the list limiting some results" do
-            subject.get_many("expressions", nil, limit: 1).should =~ [{"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}]
+            expect(subject.get_many("expressions", nil, limit: 1)).to match_array([{"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}])
           end
 
           it "returns the list limiting some reslts with an explicit key" do
-            subject.get_many("expressions", "cd34", limit: 1).should =~ [{"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}]
+            expect(subject.get_many("expressions", "cd34", limit: 1)).to match_array([{"type" => "expressions", "_id" => "2", "_rev" => 1, "a" => "b"}])
           end
         end
       end
@@ -168,7 +168,7 @@ describe Ruote::Postgres::Storage do
       end
 
       it "returns a list of id's for the matching documents" do
-        subject.ids("expressions").should =~ [ '2', '3' ]
+        expect(subject.ids("expressions")).to match_array([ '2', '3' ])
       end
     end
 
@@ -182,7 +182,7 @@ describe Ruote::Postgres::Storage do
       it "removes all the documents" do
         subject.clear
 
-        count(pg, table_name).should == 0
+        expect(count(pg, table_name)).to eq(0)
       end
     end
 
@@ -196,7 +196,7 @@ describe Ruote::Postgres::Storage do
       it "cleans the store" do
         subject.purge!
 
-        count(pg, table_name).should == 0
+        expect(count(pg, table_name)).to eq(0)
       end
     end
 
@@ -218,7 +218,7 @@ describe Ruote::Postgres::Storage do
       it "cleans the store for the given type" do
         subject.purge_type!("expressions")
 
-        count(pg, table_name, "WHERE typ='expressions'").should == 0
+        expect(count(pg, table_name, "WHERE typ='expressions'")).to eq(0)
       end
     end
   end
